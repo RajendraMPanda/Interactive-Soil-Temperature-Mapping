9fec7244997c9e3e1756fec287ebd7fba8643c11
Rajendra Mohan Panda <rmp.iit.kgp@gmail.com>
2021-04-20 12:10:22 -0400

    Update server.R

--- server.R
+++ server.R
@@ -1,14 +1,15 @@
 server<-function(input, output) {
+  #Import raster files for displaying
   STR <- raster("OK_STR.grd")
-  
+  SMR <- raster("OK_SMR.grd")
   observeEvent(input$soil, {
     Myraster <- switch(input$soil,
                        "SMR" = SMR,
                        "STR" = STR)
+    # Superimpose maps on global map
     output$soil_map <- renderLeaflet({
       leaflet() %>% addProviderTiles('Esri.WorldImagery') %>%
         addRasterImage(Myraster)
     }) 
   })
-  
-}
+  }
